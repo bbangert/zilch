54e1a627ca4499d971d944372f4b3b518b3df499
Ben Bangert <ben@groovie.org>
2011-07-11 11:29:04 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,27 @@
+=====
+zilch
+=====
+
+``zilch`` is a small extensible ZeroMQ based reporting and collecting library
+for Python. This library is inspired by `David Cramer's Sentry
+<https://github.com/dcramer/sentry>`_, but aims to implement just the core
+features in a smaller code/feature footprint with additional functionality
+provided purely by additional extension packages.
+
+
+Requirements
+============
+
+* zeromq
+
+
+License
+=======
+
+``zilch`` is offered under the MIT license.
+
+
+Authors
+=======
+
+``zilch`` is made available by `Ben Bangert`.
---
+++ setup.py
@@ -0,0 +1,31 @@
+__version__ = '0.1'
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+
+setup(name='zilch',
+      version=__version__,
+      description='ZeroMQ-based reporting and collector',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        ],
+      keywords='zeromq exceptions errors reporter collector',
+      author="Ben Bangert",
+      author_email="ben@groovie.org",
+      url="",
+      license="MIT",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require = ['pkginfo', 'Mock>=0.7', 'nose'],
+      install_requires=[
+          "pyzmq>=2.1",
+      ],
+)
